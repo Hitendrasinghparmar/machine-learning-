e7e8aaf0534d8348119597b18a6d884fd9a7163e
Kuldeep Panwar <91942775+kuldeeppanwar123@users.noreply.github.com>
2023-04-22 15:25:35 +0530

    Update Experiment-03.py

--- Experiment-03.py
+++ Experiment-03.py
@@ -97,3 +97,106 @@ print ("\nArray sorted by names:\n",
              
 print ("Array sorted by graduation year and then cgpa:\n",
                 np.sort(arr, order = ['grad_year', 'cgpa']))
+                
+
+
+
+
+# Pandas
+#Kuldeep Panwar 0827CI201096
+import pandas as pd
+import numpy as np
+ 
+# Creating empty series
+ser = pd.Series()   
+print(ser)
+# simple array
+data = np.array(['g', 'e', 'e', 'k', 's'])
+ser = pd.Series(data)
+print(ser)
+
+
+
+#Kuldeep Panwar 0827CI201096
+import pandas as pd
+   
+# Calling DataFrame constructor
+df = pd.DataFrame()
+print(df)
+ 
+# list of strings
+lst = ['kuldeep', 'jaydeep', 'mahendra', 'kunal', 
+            'nikhilesh', 'himanshu']
+   
+# Calling DataFrame constructor on list
+df = pd.DataFrame(lst)
+print(df)
+
+
+
+Matplotlib
+#Kuldeep Panwar 0827CI201096
+# importing matplotlib module
+from matplotlib import pyplot as plt
+ 
+# x-axis values
+x = [5, 2, 9, 4, 7]
+ 
+# Y-axis values
+y = [10, 5, 8, 4, 2]
+ 
+# Function to plot
+plt.plot(x,y)
+ 
+# function to show the plot
+plt.show()
+
+Bar plot : 
+# Kuldeep Panwar 0827CI201096
+# importing matplotlib module
+from matplotlib import pyplot as plt
+ 
+# x-axis values
+x = [5, 2, 9, 4, 7]
+ 
+# Y-axis values
+y = [10, 5, 8, 4, 2]
+ 
+# Function to plot the bar
+plt.bar(x,y)
+ 
+# function to show the plot
+plt.show()
+
+Histogram:
+# Kuldeep Panwar 0827CI201096
+# importing matplotlib module
+from matplotlib import pyplot as plt
+ 
+# Y-axis values
+y = [10, 5, 8, 4, 2]
+ 
+# Function to plot histogram
+plt.hist(y)
+ 
+# Function to show the plot
+plt.show()
+ 
+Scatter Plot:
+# Kuldeep Panwar 0827CI201096
+# importing matplotlib module
+from matplotlib import pyplot as plt
+ 
+# x-axis values
+x = [5, 2, 9, 4, 7]
+ 
+# Y-axis values
+y = [10, 5, 8, 4, 2]
+ 
+# Function to plot scatter
+plt.scatter(x, y)
+ 
+# function to show the plot
+plt.show()
+
+
